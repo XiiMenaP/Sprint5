2d073353df647dec8b7ab35582e10d5c3a76dea6
XiiMenaP <ixis24@yahoo.es>
2021-10-19 22:08:36 -0500

    import

---
+++ src/utils/APIinvoke.js
@@ -0,0 +1,51 @@
+import config from '../config';
+
+class APIinvoke {
+    async invokeGET(resource, queryParams){
+
+        queryParams = queryParams || []
+        const queryString = queryParams.reduce((last, q, i) => last + `${i===0? '?': "&"}${q}`, '')
+
+        const data = {
+            method: 'GET'
+        }
+        const url = `${config.api.baseUrl}${resource}${queryString}`
+        let response = (await(await fetch(url, data)).json())
+        return response
+
+    }
+
+    async invokePUT(resource, body){
+        const data = {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            Headers: {'Content-Type': 'application/json'}
+        }
+
+        const url = `${config.api.baseUrl}${resource}`
+        let response = (await(await fetch(url, data)).json())
+        return response
+    }
+
+    async invokePOST(resource, body){
+        const data = {
+            method: 'POST',
+            body: JSON.stringify(body),
+            Headers: {'content-Type':'application/json'}
+        }
+        const url = `${config.api.baseUrl}${resource}`
+        let response = (await(await fetch(url, data)).json())
+        return response
+    }
+
+    async invokeDELETE(resource){
+        const data = {
+            method: 'DELETE',
+            Headers: {'content-Type':'application/json'}
+        }
+        const url = `${config.api.baseUrl}${resource}`
+        let response = (await(await fetch(url, data)).json())
+        return response
+    }
+}
+export default new APIinvoke()
